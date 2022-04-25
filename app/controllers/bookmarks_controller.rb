00d67634b2578202326fe74a85f0d832e6e24bd7
yoann touffet <yoann.touffet@gmail.com>
2022-04-25 15:23:46 +0200

    controller bookmarks

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,26 @@
+class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
+
+end
